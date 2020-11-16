2f7d1cae5c793fa25878a6c4f1574ac7c84c1f71
Giovanni Campa <giocampa93@gmail.com>
2020-11-16 10:08:49 +0100

    Advanced plotting for analysis of selected area

--- src/background_remover.py
+++ src/background_remover.py
@@ -4,7 +4,11 @@ from matplotlib.widgets import RectangleSelector
 import cv2
 
 
-analyse_selected_area = False
+analyse_selected_area = True
+
+sigmas_hue = 1
+sigmas_value = 2
+sigmas_saturation = 3
 
 
 # -----------------------------------------------------------------------------------------
@@ -34,6 +38,17 @@ def toggle_selector(event):
     toggle_selector.RS.set_active(True)
 
 
+def range_limits(input_value, n_sigmas = 1):
+    """Returns the HSV range that we want to remove from the image
+    """
+    std = np.std(input_value)
+    mean = np.median(input_value)
+
+    value_lower = mean - std*n_sigmas
+    value_upper = mean + std*n_sigmas
+
+    return value_lower, value_upper
+
 # -----------------------------------------------------------------------------------------
 # Image capture
 
@@ -47,6 +62,9 @@ while True:
 
     k = cv2.waitKey(5) &0xFF
     if k == 27: break
+
+# This is the image we are going to substitute to the filtered areas
+original_background = frame.copy()
 
 cv2.destroyAllWindows()
 cap.release()
@@ -82,6 +100,19 @@ if analyse_selected_area:
     ax[0].set_title("Selected area")
     ax[0].set_axis_off()
 
+    hue_vals = []
+    sat_vals = []
+    val_vals = []
+
+    hue_vals.append(range_limits(area_of_interest[:,:,0], n_sigmas=sigmas_hue))
+    sat_vals.append(range_limits(area_of_interest[:,:,1], n_sigmas=sigmas_value))
+    val_vals.append(range_limits(area_of_interest[:,:,2], n_sigmas=sigmas_saturation))
+
+    for val in np.array(hue_vals).ravel(): ax[1].axvline(x = val, color="tab:red")
+    for val in np.array(sat_vals).ravel(): ax[1].axvline(x = val, color="tab:green")
+    for val in np.array(val_vals).ravel(): ax[1].axvline(x = val, color="tab:blue")
+
+
     ax[1].hist(area_of_interest[:,:,0].ravel(), label = "Hue", color = 'tab:red', alpha = 0.5, bins = 15)
     ax[1].hist(area_of_interest[:,:,1].ravel(), label = "Saturation", color = 'tab:green', alpha = 0.5, bins = 15)
     ax[1].hist(area_of_interest[:,:,2].ravel(), label = "Value", color = 'tab:blue', alpha = 0.5, bins = 15)
@@ -94,26 +125,16 @@ if analyse_selected_area:
 
 # -----------------------------------------------------------------------------------------
 # Color range selection
-# Plot the image at the selection time
 
+# Plot the image at the selection time
 area_of_interes_hsv = cv2.cvtColor(area_of_interest, cv2.COLOR_RGB2HSV)
 image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
-def range_limits(input_value, n_sigmas = 1):
-    """Returns the HSV range that we want to remove from the image
-    """
-    std = np.std(input_value)
-    mean = np.mean(input_value)
-
-    value_lower = mean - std*n_sigmas
-    value_upper = mean + std*n_sigmas
-
-    return value_lower, value_upper
 
 
-hue_lower, hue_upper = range_limits(area_of_interes_hsv[:,:,0], n_sigmas = 3)
-value_lower, value_upper = range_limits(area_of_interes_hsv[:,:,1], n_sigmas = 3)
-saturation_lower, saturation_upper = range_limits(area_of_interes_hsv[:,:,2], n_sigmas = 3)
+hue_lower, hue_upper = range_limits(area_of_interes_hsv[:,:,0], n_sigmas = sigmas_hue)
+value_lower, value_upper = range_limits(area_of_interes_hsv[:,:,1], n_sigmas = sigmas_value)
+saturation_lower, saturation_upper = range_limits(area_of_interes_hsv[:,:,2], n_sigmas = sigmas_saturation)
 
 
 lower_bound = np.array([hue_lower, value_lower, saturation_lower])
@@ -123,16 +144,24 @@ upper_bound = np.array([hue_upper, value_upper, saturation_upper])
 cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
+    image_original = frame.copy()
 
-    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)                  # Convert to Hue Saturation Value
+    mask_color = cv2.inRange(frame_hsv, lower_bound, upper_bound)       # Filter the HSV in the HSV range defined
+    # image_filtered = cv2.bitwise_not(frame, frame, mask = mask_color)   # 
 
-    mask_color = cv2.inRange(frame_hsv, lower_bound, upper_bound)
+    kernel = np.ones((5,5), np.uint8)
 
-    image_filtered = cv2.bitwise_not(frame, frame, mask = mask_color)
-    image_filtered = cv2.cvtColor(image_filtered, cv2.COLOR_HSV2RGB)
+    opening = cv2.morphologyEx(mask_color, cv2.MORPH_OPEN, kernel, iterations= 3)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=1)
 
-    cv2.imshow("Image filtered", image_filtered)
-    cv2.imshow("Image original", frame)
+    cv2.bitwise_not(frame, frame, mask = closing)   # 
+
+    oringinal_blob = cv2.bitwise_and(original_background.copy(), original_background.copy(), mask = mask_color)
+
+    cv2.imshow("Image filtered", frame)
+    cv2.imshow("Original blobs", oringinal_blob)
+    # cv2.imshow("Image original", image_original)
     k = cv2.waitKey(5) &0xFF
     if k == 27: break
 
